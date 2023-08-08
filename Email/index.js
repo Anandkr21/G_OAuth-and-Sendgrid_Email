1bf9996dbfee324ca3cefc8c4fa13b97d2ff9fee
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-08-08 23:42:27 +0530

    email & auth working

---
+++ Email/index.js
@@ -0,0 +1,24 @@
+// const nodemailer = require('nodemailer')
+
+// // transporter
+// const transporter = nodemailer.createTransport({
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     auth: {
+//         user: 'ellen.franecki98@ethereal.email',
+//         pass: 'uRXvHfvBgS3mhU26h5'
+//     }
+// });
+
+// transporter.sendMail({
+//     to:"ellen.franecki98@ethereal.email",
+//     from: "anandkumargupta526@gmail.com",
+//     subject: "Sandesh from Anand",
+//     text: "hello form nodejs"
+// })
+// .then(()=>{
+//     console.log('Email sent');
+// })
+// .catch((err) =>{
+//     console.log(err);
+// })
